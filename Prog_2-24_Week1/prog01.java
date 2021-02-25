791b68dc7ad418c928c17cb3c3eef40c30643ec4
Ehsu745 <eric17.hsu@gmail.com>
2021-02-25 16:42:47 +0800

    added wednesday's programming homework.

---
+++ Prog_2-24_Week1/prog01.java
@@ -0,0 +1,7 @@
+public class prog01
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Name: Eric Hsu / 徐上恩");
+	}
+}
---
+++ Prog_2-24_Week1/prog02.java
@@ -0,0 +1,26 @@
+public class prog02
+{
+	public static void main(String[] args)
+	{
+		System.out.println(" /¯¯¯¯¯¯\\ ");
+		System.out.println("/        \\");
+		System.out.println("\\        /");
+		System.out.println(" \\______/");
+
+		System.out.println("\\        /");
+		System.out.println(" \\______/");
+		System.out.println("+--------+");
+
+		System.out.print("\n");
+		System.out.println(" /¯¯¯¯¯¯\\ ");
+		System.out.println("/        \\");
+		System.out.println("|  STOP  |");
+		System.out.println("\\        /");
+		System.out.println(" \\______/");
+
+		System.out.print("\n");
+		System.out.println(" /¯¯¯¯¯¯\\ ");
+		System.out.println("/        \\");
+		System.out.println("+--------+");
+	}
+}
