623ed43219d9e90d1fcd90d7c1070d5ad6bfd871
Ehsu745 <eric17.hsu@gmail.com>
2021-02-27 19:59:51 +0800

    fixed err in devide.java

--- Lab_2-25_Week1/devide.java
+++ Lab_2-25_Week1/devide.java
@@ -1,12 +1,12 @@
 public class devide
 {
-	public static int ft_add(int a, int b)
+	public static int ft_devide(int a, int b)
 	{
 		System.out.print(a / b);
 		return(0);
 	}
 	public static void main(String[] args)
 	{
-		ft_add(50, 3);
+		ft_devide(50, 3);
 	}
 }
