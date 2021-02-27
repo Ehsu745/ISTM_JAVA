1028b5d942c9458bc9c32740f3a953f66c8366dc
Ehsu745 <eric17.hsu@gmail.com>
2021-02-27 21:58:53 +0800

    fix err

--- Lab_2-25_Week1/add.java
+++ Lab_2-25_Week1/add.java
@@ -2,11 +2,10 @@ public class add
 {
 	public static int ft_add(int a, int b)
 	{
-		System.out.print(a + b);
-		return(0);
+		return(a + b);
 	}
 	public static void main(String[] args)
 	{
-		ft_add(74, 36);
+		System.out.print(ft_add(74, 36));
 	}
 }
