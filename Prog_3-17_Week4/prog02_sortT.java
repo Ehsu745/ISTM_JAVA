6b9b3aece05ac4294ad62d056d6b8bd8144e4d97
Ehsu745 <eric17.hsu@gmail.com>
2021-03-23 02:05:09 +0800

    fixed every file and now prog02 and prog03 is workable

--- Prog_3-17_Week4/prog02_sortT.java
+++ Prog_3-17_Week4/prog02_sortT.java
@@ -9,6 +9,7 @@ public class prog02_sortT
 		String[] str = {"Candy", "Mango", "Gum", "Apple", "Avi"};
 		int[] brr = {};
 		String[] arr = {};
+		boolean flag = false;
 
 		System.out.println("Original int array: " + Arrays.toString(iar));
 		System.out.println("Original string array: " + Arrays.toString(str));
@@ -16,31 +17,41 @@ public class prog02_sortT
 		System.out.println("Start enter array: ");
         Scanner sc = new Scanner(System.in);
 		
-		while (sc.hasNext())
+		try
 		{
-			arr = Arrays.copyOf(arr, arr.length + 1);
-			brr = Arrays.copyOf(brr, brr.length + 1);
-			try
+			while (sc.hasNext())
 			{
+				brr = Arrays.copyOf(brr, brr.length + 1);
 				brr[brr.length - 1] = sc.nextInt();
-				System.out.println("Original: " + Arrays.toString(brr));
-				Arrays.sort(arr);
-				System.out.println("Accending: " + Arrays.toString(brr));
-				int[] crr;
-				crr = new int[brr.length];
-				for(int i = 0; i < iar.length; i++)
-					crr[i] = brr[brr.length - 1 - i];
-				System.out.println("Descending: " + Arrays.toString(crr));
-
-			} catch(Exception e)
-			{
-				arr[arr.length - 1] = sc.nextLine();
-				System.out.println("Original: " + Arrays.toString(arr));
-				Arrays.sort(arr);
-				System.out.println("Accending: " + Arrays.toString(arr));
-				Arrays.sort(arr, Collections.reverseOrder());
-				System.out.println("Descending: " + Arrays.toString(arr));
+				flag = true;
 			}
+		} catch(Exception e)
+		{	
+			while (sc.hasNext())
+			{
+				arr = Arrays.copyOf(arr, arr.length + 1);
+				arr[arr.length - 1] = sc.nextLine();
+			}
+		}
+
+		if(flag)
+		{
+			System.out.println("Original: " + Arrays.toString(brr));
+			Arrays.sort(brr);
+			System.out.println("Accending: " + Arrays.toString(brr));
+			int[] crr;
+			crr = new int[brr.length];
+			for(int i = 0; i < iar.length; i++)
+				crr[i] = brr[brr.length - 1 - i];
+			System.out.println("Descending: " + Arrays.toString(crr));
+		}
+		else
+		{
+			System.out.println("Original: " + Arrays.toString(arr));
+			Arrays.sort(arr);
+			System.out.println("Accending: " + Arrays.toString(arr));
+			Arrays.sort(arr, Collections.reverseOrder());
+			System.out.println("Descending: " + Arrays.toString(arr));
 		}
 	}
 }
